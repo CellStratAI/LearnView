1091abdc795b5f495dc40a058d22efd0f484bbfd
Vishal Ranka <vishalranka3@gmail.com>
2024-04-04 03:35:34 +0530

    landin Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,42 +1,32 @@
-// pages/index.tsx
+import React from "react";
 
-import Head from "next/head";
-import Link from "next/link";
+import About from "../components/About";
+// import Analytics from "../components/Analytics";
+import Features from "../components/Features";
+import Header from "../components/Header";
+import MainHero from "../components/MainHero";
+import MainHeroImage from "../components/MainHeroImage";
+import Pricing from "../components/Pricing";
+import Product from "../components/Product";
 
-const Home: React.FC = () => {
+const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>LearnView - Open Source Learning App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to <span className="text-blue-500">LearnView</span>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          An open source learning app for all your educational needs.
-        </p>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        Open Source Project by{" "}
-        <Link
-          href="https://cellstrat.com"
-          style={{
-            color: "cyan",
-            marginLeft: "0.2rem",
-          }}
-          target="_blank"
-        >
-          {" "}
-          Cellstrat
-        </Link>
-      </footer>
+    <div className="bg-gradient-to-r from-gray-900 to-black grid gap-y-16 overflow-hidden">
+      <div className={`relative bg-transaprent`}>
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-transparent sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <Header />
+            <MainHero />
+          </div>
+        </div>
+        <MainHeroImage />
+      </div>
+      <Product />
+      <Features />
+      <About />
+      {/* <Analytics /> */}
     </div>
   );
 };
 
-export default Home;
+export default App;
